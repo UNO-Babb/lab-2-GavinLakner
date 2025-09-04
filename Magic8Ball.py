5bf99c257a82d0d25a1476ee8133ce432e406176
GavinLakner <gavinlakner@icloud.com>
2025-09-04 17:16:22 -0500

    Update Magic8Ball.py

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -1,7 +1,7 @@
 #Magic8Ball.py
-#Name:
-#Date:
-#Assignment:
+#Name: Gavin Lakner
+#Date: 9/4/2025
+#Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
 import random
@@ -10,9 +10,15 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-
+answers = [ "List answer" , "another answer" ,  ]
   #Answer question randomly with one of the options from your earlier list.
 
+length = len(answer) 
+r = random.random()
+r = int(r) 
 
+print(r)
+response = answers[r]
+print(response)
 if __name__ == '__main__':
   main()
