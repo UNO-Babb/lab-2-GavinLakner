d9833e74e64aeea499d821f4ea250f2d7e39dd1b
GavinLakner <gavinlakner@icloud.com>
2025-09-04 17:22:49 -0500

    Done Magic8Ball.py

--- Magic8Ball.py
+++ Magic8Ball.py
@@ -10,7 +10,9 @@ def main():
   #Create a list of your responses.
   print("Magic 8 Ball")
   #Prompt the user for their question.
-answers = [ "List answer" , "another answer" ,  ]
+answers = [ "Why not" , "Go For IT" , "Might As Well" , "Heck No" , "That's A Silly Question" , "You Know The Answer" , "Trust Your Gut" , 
+           "Meh" , "Woah There" , "Go Fishing Instead" , "Let Me Think About It" , "I'd Reconsider" , "Maybe Next Time" , "Goodluck" , "Ask Ai" , 
+           "Flip A Coin" , "Try Meditating For Your Answer" , "Does Money Grow On Trees?" ,] 
   #Answer question randomly with one of the options from your earlier list.
 
 length = len(answer) 
