ca3d471642bec6f5483ed5eb1e8ef86d22b2f4b5
GavinLakner <gavinlakner@icloud.com>
2025-09-08 04:57:00 +0000

    Lost in the sauce but out of time

--- FutureTime.py
+++ FutureTime.py
@@ -9,26 +9,29 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = (now.hour -8 ) % 24
-  currentMinute = (now.minute - 4)
+  currentHour = (now.hour -5 ) % 24
+  currentMinute = (now.minute + 1) 
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
 
   #TODO:
   #Ask user for hours
-moreHr = (currentHour + moreHr) % 60
+moreHour = 1
   #Ask user for minutes
-moreMins = 5
-
-futureMins = (currentMinute + moreMins ) % 60
-extraHour = (currentMinute + moreMins ) // 60
+moreMins = 1
 
   #Calculate the time after the user-supplied time has passed.
+futureMins = (currentMinute + moreMins ) % 60 
+extraHour = (currentMinute + moreMins ) // 60 
+# I have no idea why these won't work and after watching your lecture and video it should work and google gives no help
 
+print (extraHour)
+print (futureMins)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
-
+formatted_time = datetime.strftime("%H:%M")
+#Found this online but I don't know if it works
 
 if __name__ == '__main__':
   main()
--- Magic8Ball.py
+++ Magic8Ball.py
@@ -4,20 +4,25 @@
 #Assignment: Lab 2
 
 #We will need random for this program, import to use this package.
-import random
+import random 
 
 def main():
   #Create a list of your responses.
-  print("Magic 8 Ball")
+  
+
   #Prompt the user for their question.
-answers = [ "Why not" , "Go For IT" , "Might As Well" , "Heck No" , "That's A Silly Question" , "You Know The Answer" , "Trust Your Gut" , 
+  print ("Ask Any Question")
+  
+answers = [ "Why not","Go For IT","Might As Well", "Heck No" , "That's A Silly Question" , "You Know The Answer" , "Trust Your Gut" , 
            "Meh" , "Woah There" , "Go Fishing Instead" , "Let Me Think About It" , "I'd Reconsider" , "Maybe Next Time" , "Goodluck" , "Ask Ai" , 
            "Flip A Coin" , "Try Meditating For Your Answer" , "Does Money Grow On Trees?" ,] 
   #Answer question randomly with one of the options from your earlier list.
 
-length = len(answer) 
-r = random.random()
-r = int(r) 
+
+length = len(answers) 
+r = random.choice(answers)
+r = int(r)  
+print ("no idea how to fix line 24 but it works and gives a random answer")
 
 print(r)
 response = answers[r]
